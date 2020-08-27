7bd5ac776d3a3b0d5f362e65cfe7202fe8060d61
如果的如果 <admin@fosuss.com>
2020-08-27 15:16:35 +0800

    改版测试

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # doctrine-annotations
+
+## 版权声明
+本项目修改自 [doctrine/annotation](https://github.com/doctrine/annotations) , 原项目开源协议为MIT协议。
---
+++ tests/AnnotationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace EasySwoole\DoctrineAnnotation\Tests;
+
+
+use EasySwoole\DoctrineAnnotation\AnnotationReader;
+use PHPUnit\Framework\TestCase;
+use EasySwoole\DoctrineAnnotation\Tests\Tag\PropertyTag;
+use EasySwoole\DoctrineAnnotation\Tests\Tag\NonePropertyTag;
+
+
+class AnnotationTest extends TestCase
+{
+    /**
+     * @PropertyTag(input={"code":2})
+     * @PropertyTag(input=r"{"code":2,"result":[{"name":1}]}")
+     */
+    private $a;
+
+    /**
+     * @NonePropertyTag({"code":2})
+     * @NonePropertyTag(r"{"code":2,"result":[{"name":1}]}")
+     */
+    private $b;
+
+    private $ref;
+    private $reader;
+
+    function __construct($name = null, array $data = [], $dataName = '')
+    {
+        $this->ref = new \ReflectionClass(static::class);
+        $this->reader = new AnnotationReader();
+        parent::__construct($name, $data, $dataName);
+    }
+
+    function testProperty()
+    {
+        $ret = $this->reader->getPropertyAnnotations($this->ref->getProperty('a'));
+        $this->assertIsArray($ret);
+        $this->assertEquals(2,count($ret));
+        $this->assertEquals(["code"=>2],$ret[0]->input);
+        $this->assertEquals('{"code":2,"result":[{"name":1}]}',$ret[1]->input);
+    }
+
+    function testNoneProperty()
+    {
+        $ret = $this->reader->getPropertyAnnotations($this->ref->getProperty('b'));
+        $this->assertIsArray($ret);
+        $this->assertEquals(2,count($ret));
+        $this->assertEquals(["code"=>2],$ret[0]->value);
+        $this->assertEquals('{"code":2,"result":[{"name":1}]}',$ret[1]->value);
+    }
+}
